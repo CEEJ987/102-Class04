ebb8334518bdfaf059ef0b0fdbf4d1f79d0c2362
CEEJ987 <123979158+CEEJ987@users.noreply.github.com>
2023-02-15 21:25:54 -0600

    update Assignments

--- script.js
+++ script.js
@@ -6,14 +6,40 @@ let lesson = function (x){
     
 }
 
-lesson(a)
-12()
+
+
 
 function whodat(){
     let name = prompt("what is your name?")
     //let name = console.Readline();
     if (name != 'DJ'){
         alert("ERROR");
-    } else 
+    } 
+
+   
 }
+
+function validatePass() {
+    
+    let password = document.getElementById("RegPass").value;
+    alert(password.length);
+    //while (password.length <5 || password.length > 15) {
+        if (password.length > 4){
+            alert("Password not valid");
+
+        }
+        if (password.length < 15){
+            alert("Invalid Password");
+        }
+
+        if (username.length > 2){
+            alert("Invalid Name")
+        }
+        //alert("Hello! I am an alert box!");
+   // }
+}
+function greet(name) {
+    return "Hello, " + name;
+  }
+  greet("Grace Hopper");
 //console.log(b);
